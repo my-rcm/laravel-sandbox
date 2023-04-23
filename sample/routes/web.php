77359dcca5819d0b8eaf609b0914dc530ad5b465
Masahiro <m.yamashita.apillo@gmail.com>
2023-04-23 11:18:14 +0900

    feat: chapter1の反映

--- sample/routes/web.php
+++ sample/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/register', [App\HTTP\Controllers\RegisterController::class, 'create'])->middleware('guest')->name('register');
+Route::post('/register', [App\HTTP\Controllers\RegisterController::class, 'store'])->middleware('guest');
+Route::get('/login', [App\HTTP\Controllers\LoginController::class, 'index'])->middleware('guest');
+Route::post('/login', [App\HTTP\Controllers\LoginController::class, 'authenticate'])->middleware('guest');
+Route::get('/logout', [App\HTTP\Controllers\LoginController::class, 'logout'])->middleware('auth')->name('logout');
